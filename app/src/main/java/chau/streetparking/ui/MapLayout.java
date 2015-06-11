9eaa27b8b569bd2665b04b025621c025e15b0f74
sukhoi344 <thchau1991>
2015-06-11 17:51:20 -0400

    Add CurtainView

--- app/src/main/java/chau/streetparking/ui/MapLayout.java
+++ app/src/main/java/chau/streetparking/ui/MapLayout.java
@@ -5,25 +5,25 @@ import android.content.Context;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.FrameLayout;
-import android.widget.LinearLayout;
-import android.widget.SeekBar;
-import android.widget.TextView;
+
+import com.appyvet.rangebar.RangeBar;
 
 import chau.streetparking.R;
+import chau.streetparking.ui.curtain.CurtainView;
+import chau.streetparking.ui.curtain.ICurtainViewBase;
 
 /**
  * Created by Chau Thai on 6/9/2015.
  */
 public class MapLayout extends FrameLayout {
     // Widgets
-    private LinearLayout requestAddLayout;
-    private LinearLayout scheduleCancelLayout;
-    private LinearLayout seekBarLayout;
-    private VerticalSeekBar seekBar;
-    private TextView rangeIndicator;
+    private ViewGroup requestAddLayout;
+    private CurtainView curtainView;
+    private RangeBar seekBar;
 
     public MapLayout(Context context) {
         super(context);
@@ -46,21 +46,25 @@ public class MapLayout extends FrameLayout {
         init(context);
     }
 
-    public void setSeekBarListener(SeekBar.OnSeekBarChangeListener listener) {
+    public void setSeekBarListener(RangeBar.OnRangeBarChangeListener listener) {
         if (seekBar != null && listener != null) {
-            seekBar.setOnSeekBarChangeListener(listener);
+            seekBar.setOnRangeBarChangeListener(listener);
         }
     }
 
+    /**
+     * Change UI when the user selects "REQUEST" button
+     */
     public void showNext() {
         hideRequestAdd();
         showScheduleCancel();
-        showSeekBar();
     }
 
+    /**
+     * Change UI when the user selects "CANCEL" button
+     */
     public void cancel() {
         hideScheduleCancel();
-        hideSeekBar();
         showRequestAdd();
     }
 
@@ -72,51 +76,38 @@ public class MapLayout extends FrameLayout {
     }
 
     private void hideRequestAdd() {
-        Animation animation = AnimationUtils.loadAnimation(getContext(), R.anim.slide_out_left);
-        animation.setDuration(getContext().getResources().getInteger(android.R.integer.config_shortAnimTime));
-        requestAddLayout.startAnimation(animation);
+//        Animation animation = AnimationUtils.loadAnimation(getContext(), R.anim.slide_out_left);
+//        animation.setDuration(getContext().getResources().getInteger(android.R.integer.config_shortAnimTime));
+//        requestAddLayout.startAnimation(animation);
         requestAddLayout.setVisibility(View.GONE);
     }
 
     private void showScheduleCancel() {
-        Animation animation = AnimationUtils.loadAnimation(getContext(), R.anim.slide_in_right);
-        animation.setDuration(getContext().getResources().getInteger(android.R.integer.config_shortAnimTime));
-        scheduleCancelLayout.startAnimation(animation);
-        scheduleCancelLayout.setVisibility(View.VISIBLE);
+//        Animation animation = AnimationUtils.loadAnimation(getContext(), R.anim.slide_in_right);
+//        animation.setDuration(getContext().getResources().getInteger(android.R.integer.config_shortAnimTime));
+//        curtainView.startAnimation(animation);
+        curtainView.setVisibility(View.VISIBLE);
+//        curtainView.setCurtainStatus(ICurtainViewBase.CurtainStatus.OPENED);
+
     }
 
     private void hideScheduleCancel() {
         Animation animation = AnimationUtils.loadAnimation(getContext(), R.anim.slide_out_right);
         animation.setDuration(getContext().getResources().getInteger(android.R.integer.config_shortAnimTime));
-        scheduleCancelLayout.startAnimation(animation);
-        scheduleCancelLayout.setVisibility(View.GONE);
-    }
-
-    private void showSeekBar() {
-        Animation animation = AnimationUtils.loadAnimation(getContext(), R.anim.slide_in_right);
-        animation.setDuration(getContext().getResources().getInteger(android.R.integer.config_shortAnimTime));
-        seekBarLayout.startAnimation(animation);
-        seekBarLayout.setVisibility(View.VISIBLE);
-    }
-
-    private void hideSeekBar() {
-        Animation animation = AnimationUtils.loadAnimation(getContext(), R.anim.slide_out_right);
-        animation.setDuration(getContext().getResources().getInteger(android.R.integer.config_shortAnimTime));
-        seekBarLayout.startAnimation(animation);
-        seekBarLayout.setVisibility(View.GONE);
+        curtainView.startAnimation(animation);
+        curtainView.setVisibility(View.GONE);
     }
 
     private void init(Context context) {
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         inflater.inflate(R.layout.include_map_layout, this, true);
         getWidgets();
+        seekBar.setSeekPinByValue(100);
     }
 
     private void getWidgets() {
-        requestAddLayout = (LinearLayout) findViewById(R.id.request_add_layout);
-        scheduleCancelLayout = (LinearLayout) findViewById(R.id.schedule_cancel_layout);
-        seekBarLayout = (LinearLayout) findViewById(R.id.seek_bar_layout);
-        seekBar = (VerticalSeekBar) findViewById(R.id.seek_bar);
-        rangeIndicator = (TextView) findViewById(R.id.range_indicator);
+        requestAddLayout = (ViewGroup) findViewById(R.id.request_add_layout);
+        curtainView = (CurtainView) findViewById(R.id.curtain_view);
+        seekBar = (RangeBar) findViewById(R.id.seek_bar);
     }
 }
