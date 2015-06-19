72faa8e5af688e9fb4f690eee19102617b9fb3d0
sukhoi344 <conchimbayra@gmail.com>
2015-06-19 17:17:11 -0400

    Add PaymentActivity

--- app/src/main/java/chau/streetparking/ui/ProfileActivity.java
+++ app/src/main/java/chau/streetparking/ui/ProfileActivity.java
@@ -1,12 +1,23 @@
 package chau.streetparking.ui;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
 import chau.streetparking.R;
 
 /**
  * Created by Chau Thai on 6/17/2015.
  */
 public class ProfileActivity extends ColoredBarActivity {
+    private EditText    etFirstName,
+                        etLastName,
+                        etMobile,
+                        etEmail;
+    private Button      btnSave;
 
     @Override
     protected int getLayout() {
@@ -21,5 +32,52 @@ public class ProfileActivity extends ColoredBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWidgets();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_profile, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_edit:
+                setEditEnable(true);
+                return true;
+            case R.id.menu_sign_out:
+                signOut();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    /**
+     * Save button clicked event callback
+     */
+    public void onSaveClicked(View v) {
+    }
+
+    private void setEditEnable(boolean enable) {
+        etEmail.setEnabled(enable);
+        etFirstName.setEnabled(enable);
+        etLastName.setEnabled(enable);
+        etMobile.setEnabled(enable);
+        btnSave.setVisibility(enable? View.VISIBLE : View.GONE);
+    }
+
+    private void signOut() {
+        finish();
+    }
+
+    private void getWidgets() {
+        etEmail = (EditText) findViewById(R.id.email);
+        etFirstName = (EditText) findViewById(R.id.first_name);
+        etLastName = (EditText) findViewById(R.id.last_name);
+        etMobile = (EditText) findViewById(R.id.mobile);
+        btnSave = (Button) findViewById(R.id.save);
     }
 }
