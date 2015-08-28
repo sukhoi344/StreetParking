c01af62d92e4a9df99d7eab0097eb9aa66e48634
sukhoi344 <conchimbayra@gmail.com>
2015-08-28 17:23:06 -0400

    Added delete photo button

--- app/src/main/java/chau/streetparking/ui/PhotosAdapter.java
+++ app/src/main/java/chau/streetparking/ui/PhotosAdapter.java
@@ -6,6 +6,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import com.nostra13.universalimageloader.core.ImageLoader;
@@ -23,11 +24,13 @@ public class PhotosAdapter extends RecyclerView.Adapter {
 
     private static class ViewHolder extends RecyclerView.ViewHolder {
         ImageView imageView;
+        Button btnRemove;
         View view;
 
         public ViewHolder(View v) {
             super(v);
             imageView = (ImageView) v.findViewById(R.id.item_photo);
+            btnRemove = (Button) v.findViewById(R.id.item_remove);
             view = v;
         }
     }
@@ -40,7 +43,7 @@ public class PhotosAdapter extends RecyclerView.Adapter {
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View v = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.photo_row, parent, false);
+                .inflate(R.layout.photo_row_deleteable, parent, false);
         ViewHolder holder = new ViewHolder(v);
         return holder;
     }
@@ -52,6 +55,14 @@ public class PhotosAdapter extends RecyclerView.Adapter {
             final ViewHolder viewHolder = (ViewHolder) holder;
 
             ImageLoader.getInstance().displayImage(uri.toString(), viewHolder.imageView);
+
+            viewHolder.btnRemove.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    dataSet.remove(uri);
+                    notifyDataSetChanged();
+                }
+            });
         }
     }
 
