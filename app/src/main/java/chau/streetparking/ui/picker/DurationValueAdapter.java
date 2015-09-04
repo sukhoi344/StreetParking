acdf0d9f8fd472496d69dc4ce02d89c67b2e429c
sukhoi344 <conchimbayra@gmail.com>
2015-09-04 15:55:32 -0400

    Fixed Duration bug

--- app/src/main/java/chau/streetparking/ui/picker/DurationValueAdapter.java
+++ app/src/main/java/chau/streetparking/ui/picker/DurationValueAdapter.java
@@ -77,7 +77,7 @@ public class DurationValueAdapter extends RecyclerView.Adapter<DurationViewHolde
 
     public void addData() {
         int size = dataSet.size();
-        for (int i = size; i < size + 100; i++) {
+        for (int i = size + 1; i < size + 100; i++) {
             dataSet.add(i);
         }
     }
