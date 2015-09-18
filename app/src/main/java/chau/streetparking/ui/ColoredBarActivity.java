1aec48d72496287d99fad3c8b9b2cfdee0a66865
sukhoi344 <conchimbayra@gmail.com>
2015-09-18 16:05:40 -0400

    Replace the '+' at the end of the dependencies for consistency.

--- app/src/main/java/chau/streetparking/ui/ColoredBarActivity.java
+++ app/src/main/java/chau/streetparking/ui/ColoredBarActivity.java
@@ -8,7 +8,6 @@ import android.view.View;
 import com.readystatesoftware.systembartint.SystemBarTintManager;
 
 import chau.streetparking.R;
-import chau.streetparking.util.Logger;
 
 /**
  * Activity with colored status bar and action bar.
